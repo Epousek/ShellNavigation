97604a03b557b424be425a3fdaee173a137917f1
Epousek <epousek@seznam.cz>
2021-10-26 14:36:00 +0200

    Add project files.

---
+++ ShellApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ShellApp.ViewModels;
+using ShellApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ShellApp
+{
+  public partial class AppShell : Xamarin.Forms.Shell
+  {
+    public AppShell()
+    {
+      InitializeComponent();
+      Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+      Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+    }
+
+    private async void OnMenuItemClicked(object sender, EventArgs e)
+    {
+      await Shell.Current.GoToAsync("//LoginPage");
+    }
+  }
+}
---
+++ ShellApp/Views/TeachersPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShellApp.Views
+{
+  public class Teacher
+  {
+    public string Degree { get; set; }
+    public string Name { get; set; }
+  }
+
+  public partial class TeachersPage : ContentPage
+  {
+    public static ObservableCollection<Teacher> TeachersList { get; set; }
+
+    public TeachersPage()
+    {
+      InitializeComponent();
+
+      TeachersList = new ObservableCollection<Teacher>(
+        new Teacher[]
+        {
+          new Teacher { Degree = "Ing.", Name = "Radko Sáblík" },
+          new Teacher { Degree = "Mgr.", Name = "Zdeněk Nechanický" },
+          new Teacher { Degree = "Ing.", Name = "Pavel Švec" }
+        });
+
+      TeachersLV.ItemsSource = TeachersList;
+    }
+
+    private void AddTeacherBut_Clicked(object sender, EventArgs e)
+      => TeachersList.Add(new Teacher { Degree = "Mgr.", Name = "Tomáš Tkáč" });
+  }
+}
